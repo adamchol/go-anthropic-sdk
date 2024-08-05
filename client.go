f551577c3b5cecbe4365b95ab2177d51a85006f4
Richard Shank <itsrichardshank@gmail.com>
2024-08-05 21:09:48 +0200

    feat: add basic functionality for standard use of Messages API

---
+++ client.go
@@ -0,0 +1,114 @@
+package anthropic
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	utils "github.com/adamchol/go-anthropic-sdk/internal"
+)
+
+type Client struct {
+	config ClientConfig
+
+	requestBuilder utils.RequestBuilder
+}
+
+func NewClientWithConfig(config ClientConfig) *Client {
+	return &Client{
+		config:         config,
+		requestBuilder: utils.NewRequestBuilder(),
+	}
+}
+
+func NewClient(apiKey string) *Client {
+	return NewClientWithConfig(DefaultConfig(apiKey))
+}
+
+type requestOptions struct {
+	body   any
+	header http.Header
+}
+
+type requestOption func(*requestOptions)
+
+func withBody(body any) requestOption {
+	return func(args *requestOptions) {
+		args.body = body
+	}
+}
+
+func withContentType(contentType string) requestOption {
+	return func(args *requestOptions) {
+		args.header.Set("Content-Type", contentType)
+	}
+}
+
+func (c *Client) newRequest(ctx context.Context, method, url string, setters ...requestOption) (*http.Request, error) {
+	args := &requestOptions{
+		body:   nil,
+		header: make(http.Header),
+	}
+	for _, setter := range setters {
+		setter(args)
+	}
+	req, err := c.requestBuilder.Build(ctx, method, url, args.body, args.header)
+	if err != nil {
+		return nil, err
+	}
+	c.setCommonHeaders(req)
+	return req, nil
+}
+
+func (c *Client) sendRequest(request *http.Request, v any) error {
+	resp, err := c.config.HTTPClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if isFailureStatusCode(resp) {
+		// TODO: handle errors properly
+		return handleErrorResponse(resp)
+	}
+
+	return json.NewDecoder(resp.Body).Decode(v)
+}
+
+func (c *Client) setCommonHeaders(req *http.Request) {
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("anthropic-version", string(c.config.APIVersion))
+	req.Header.Set("x-api-key", c.config.authToken)
+}
+
+func (c *Client) fullURL(suffix string) string {
+	return fmt.Sprintf("%s%s", c.config.BaseUrl, suffix)
+}
+
+func isFailureStatusCode(response *http.Response) bool {
+	return response.StatusCode < http.StatusOK || response.StatusCode >= http.StatusBadRequest
+}
+
+type ErrorResponse struct {
+	Type  string    `json:"type"`
+	Error *APIError `json:"error"`
+}
+
+type APIError struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
+func handleErrorResponse(resp *http.Response) error {
+	var errResp ErrorResponse
+
+	err := json.NewDecoder(resp.Body).Decode(&errResp)
+	if err != nil || errResp.Error == nil {
+		return err
+	}
+
+	return errors.New(errResp.Error.Message)
+}
---
+++ message.go
@@ -0,0 +1,212 @@
+package anthropic
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+const (
+	MessageRoleAssistant = "assistant"
+	MessageRoleUser      = "user"
+)
+
+const messagesSuffix = "/messages"
+
+var (
+	ErrContentFieldsMisused             = errors.New("can't use both Content and ContentBlocks properties simultaneously")
+	ErrChatCompletionStreamNotSupported = errors.New("streaming is not supported with this method, please use CreateChatCompletionStream") //nolint:lll
+	ErrModelNotAvailable                = errors.New("this model is not available for Anthropic Messages API")
+)
+
+const (
+	TextContentObjectType       = "text"
+	ImageContentObjectType      = "image"
+	ToolUseContentObjectType    = "tool_use"
+	ToolResultContentObjectType = "tool_result"
+)
+
+type ContentBlock struct {
+	Type string `json:"type"`
+
+	// For Text type
+	Text string `json:"text,omitempty"`
+
+	// For Image type
+	Source ImageSource `json:"source,omitempty"`
+
+	// For Tool Use type
+	Id    string                 `json:"id,omitempty"`
+	Name  string                 `json:"name,omitempty"`
+	Input map[string]interface{} `json:"input,omitempty"`
+
+	// For Tool Result type
+	ToolUseId         string `json:"tool_use_id,omitempty"`
+	IsError           bool   `json:"is_error,omitempty"`
+	ToolResultContent []struct {
+		Type string `json:"type"`
+
+		// For Text type
+		Text string `json:"text,omitempty"`
+
+		// For Image type
+		Source ImageSource `json:"source,omitempty"`
+	} `json:"content,omitempty"`
+}
+
+const ImageSourceType = "base64"
+const (
+	ImageJPEGMediaType = "image/jpeg"
+	ImagePNGMediaType  = "image/png"
+	ImageGIFMediaType  = "image/gif"
+	ImageWebPMediaType = "image/webp"
+)
+
+type ImageSource struct {
+	Type      string `json:"type"`
+	MediaType string `json:"media_type"`
+	Data      string `json:"data"`
+}
+
+type InputMessage struct {
+	Role          string `json:"role"`
+	Content       string `json:"content"`
+	ContentBlocks []ContentBlock
+}
+
+func (m InputMessage) MarshalJSON() ([]byte, error) {
+	if m.Content != "" && m.ContentBlocks != nil {
+		return nil, ErrContentFieldsMisused
+	}
+
+	if len(m.ContentBlocks) > 0 {
+		msg := struct {
+			Role          string         `json:"role"`
+			Content       string         `json:"-"`
+			ContentBlocks []ContentBlock `json:"content"`
+		}(m)
+		return json.Marshal(msg)
+	}
+
+	msg := struct {
+		Role          string         `json:"role"`
+		Content       string         `json:"content"`
+		ContentBlocks []ContentBlock `json:"-"`
+	}(m)
+	return json.Marshal(msg)
+}
+
+func (m *InputMessage) UnmarshalJSON(bs []byte) error {
+	msg := InputMessage{}
+
+	if err := json.Unmarshal(bs, &msg); err == nil {
+		*m = msg
+		return nil
+	}
+
+	objectMsg := struct {
+		Role          string `json:"role"`
+		Content       string
+		ContentBlocks []ContentBlock `json:"content"`
+	}{}
+
+	if err := json.Unmarshal(bs, &objectMsg); err != nil {
+		return err
+	}
+
+	*m = InputMessage(objectMsg)
+	return nil
+}
+
+const (
+	Claude35SonnetModel = "claude-3-5-sonnet-20240620"
+	Claude3OpusModel    = "claude-3-opus-20240229"
+	Claude3SonnetModel  = "claude-3-sonnet-20240229"
+	Claude3HaikuModel   = "claude-3-haiku-20240307"
+)
+
+type MessageRequestMetadata struct {
+	UserId string `json:"user_id,omitempty"`
+}
+
+type MessageRequest struct {
+	Model     string         `json:"model"`
+	Messages  []InputMessage `json:"messages"`
+	MaxTokens int            `json:"max_tokens"`
+
+	Temperature   int                    `json:"temperature,omitempty"`
+	StopSequences []string               `json:"stop_sequences,omitempty"`
+	Metadata      MessageRequestMetadata `json:"metadata,omitempty"`
+	Stream        bool                   `json:"stream,omitempty"`
+	System        string                 `json:"system,omitempty"`
+	TopK          int                    `json:"top_k,omitempty"`
+	TopP          int                    `json:"top_p,omitempty"`
+
+	Tools      []Tool      `json:"tools,omitempty"`
+	ToolChoice *ToolChoice `json:"tool_choice,omitempty"`
+}
+
+type Tool struct {
+	Name        string                 `json:"name"`
+	Description string                 `json:"description,omitempty"`
+	InputSchema map[string]interface{} `json:"input_schema"`
+}
+
+const ObjectToolInputSchemaType = "object"
+
+type ToolInputSchema struct {
+	Type       string                 `json:"type"`
+	Properties map[string]interface{} `json:"properties,omitempty"`
+}
+
+const (
+	AutoToolChoiceType = "auto"
+	AnyToolChoiceType  = "any"
+	ToolToolChoiceType = "tool"
+)
+
+type ToolChoice struct {
+	Type string `json:"type"`
+	Name string `json:"name,omitempty"`
+}
+
+type StopReason string
+
+const (
+	StopReasonEndTurn      StopReason = "end_turn"
+	StopReasonMaxTokens    StopReason = "max_tokens"
+	StopReasonStopSequence StopReason = "stop_sequence"
+	StopReasonToolUser     StopReason = "tool_use"
+)
+
+type MessageResponse struct {
+	ID           string         `json:"id"`
+	Type         string         `json:"type"`
+	Content      []ContentBlock `json:"content"`
+	Role         string         `json:"role"`
+	Model        string         `json:"model"`
+	StopReason   StopReason     `json:"stop_reason"`
+	StopSequence string         `json:"stop_sequence,omitempty"`
+	Usage        Usage          `json:"usage"`
+}
+
+type Usage struct {
+	InputTokens  int `json:"input_tokens"`
+	OutputTokens int `json:"output_tokens"`
+}
+
+func (c *Client) CreateMessage(ctx context.Context, request MessageRequest) (response MessageResponse, err error) {
+	if request.Stream {
+		err = ErrChatCompletionStreamNotSupported
+		return
+	}
+
+	req, err := c.newRequest(context.Background(), http.MethodPost, c.fullURL(messagesSuffix), withBody(request))
+	if err != nil {
+		return
+	}
+
+	err = c.sendRequest(req, &response)
+	return
+}
